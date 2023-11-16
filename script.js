2e136cadce9df8ce98160f81ab27e1d67f000a2b
ChernyHleb <popovarlol@gmail.com>
2023-11-16 19:36:13 +0300

    started working on products filter

---
+++ script.js
@@ -0,0 +1,56 @@
+let products = {
+    data : [
+        {
+            product_name: "Hamedorea",
+            category: "houseplant",
+            price: "9",
+            image: "img/flowers/large_Без_имени-1.jpg"
+        },
+        {
+            product_name: "Dracaena",
+            category: "houseplant",
+            price: "14",
+            image: "img/flowers/large_Без_имени-2.jpg"
+        },
+        {
+            product_name: "Epipremnum",
+            category: "houseplant",
+            price: "7",
+            image: "img/flowers/large_Без_имени-3.jpg"
+        },
+        {
+            product_name: "Zamioculcas",
+            category: "houseplant",
+            price: "16",
+            image: "img/flowers/large_Без_имени-4.jpg"
+        },
+        {
+            product_name: "Nolina",
+            category: "houseplant",
+            price: "9",
+            image: "img/flowers/large_Без_имени-5.jpg"
+        },
+        {
+            product_name: "Spatifillum",
+            category: "houseplant",
+            price: "15",
+            image: "img/flowers/large_Без_имени-6.jpg"
+        },
+        {
+            product_name: "Yucca palm",
+            category: "houseplant",
+            price: "10",
+            image: "img/flowers/large_Без_имени-7.jpg"
+        },
+        {
+            product_name: "Ficus Ali",
+            category: "houseplant",
+            price: "8",
+            image: "img/flowers/large_Без_имени-8.jpg"
+        }
+    ]
+};
+
+for (const product of products) {
+    
+}

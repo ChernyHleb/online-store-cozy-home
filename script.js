80df5fdf55dcb1f88220b5c5dd5c2419cb26cf81
ChernyHleb <popovarlol@gmail.com>
2023-11-17 16:47:53 +0300

    :D

--- script.js
+++ script.js
@@ -3,75 +3,96 @@ let products = {
         {
             product_name: "Hamedorea",
             category: "houseplant",
+            type:"ornamental_leaf_plant",
             price: "9",
-            image: "img/flowers/large_Без_имени-1.jpg"
+            image: "img/flowers/large_Без_имени-1.jpg",
+            manufacturer_country: "India"
         },
         {
             product_name: "Dracaena",
             category: "houseplant",
+            type: "ornamental_leaf_plant",
             price: "14",
-            image: "img/flowers/large_Без_имени-2.jpg"
+            image: "img/flowers/large_Без_имени-2.jpg",
+            manufacturer_country: "India"
         },
         {
             product_name: "Epipremnum",
             category: "houseplant",
+            type: "ornamental_leaf_plant",
             price: "7",
-            image: "img/flowers/large_Без_имени-3.jpg"
+            image: "img/flowers/large_Без_имени-3.jpg",
+            manufacturer_country: "India"
         },
         {
             product_name: "Zamioculcas",
             category: "houseplant",
+            type: "perennial_plant",
             price: "16",
-            image: "img/flowers/large_Без_имени-4.jpg"
+            image: "img/flowers/large_Без_имени-4.jpg",
+            manufacturer_country: "Netherlands"
         },
         {
             product_name: "Nolina",
             category: "houseplant",
+            type: "perennial_plant",
             price: "9",
-            image: "img/flowers/large_Без_имени-5.jpg"
+            image: "img/flowers/large_Без_имени-5.jpg",
+            manufacturer_country: "Netherlands"
         },
         {
             product_name: "Spatifillum",
             category: "houseplant",
+            type: "flowering_plant",
             price: "15",
-            image: "img/flowers/large_Без_имени-6.jpg"
+            image: "img/flowers/large_Без_имени-6.jpg",
+            manufacturer_country: "China"
         },
         {
             product_name: "Yucca palm",
             category: "houseplant",
+            type: "ornamental_leaf_plant",
             price: "10",
-            image: "img/flowers/large_Без_имени-7.jpg"
+            image: "img/flowers/large_Без_имени-7.jpg",
+            manufacturer_country: "China"
         },
         {
             product_name: "Ficus Ali",
             category: "houseplant",
+            type: "ornamental_leaf_plant",
             price: "8",
-            image: "img/flowers/large_Без_имени-8.jpg"
+            image: "img/flowers/large_Без_имени-8.jpg",
+            manufacturer_country: "China"
         }
     ]
 };
 
-for (const item of products.data) {
-    let product = document.createElement("div");
-    product.classList.add("product", item.category);
+filterChange("ALL");
 
-    let image = document.createElement("img");
-    image.classList.add("product_image")
-    image.setAttribute("src", item.image);
-    product.appendChild(image);
+function filterChange(value) {
+    for (const item of products.data) {
 
-    let description = document.createElement("div");
-    description.classList.add("product_description");
+        let product = document.createElement("div");
+        product.classList.add("product", item.category);
 
-    let span = document.createElement("span");
-    span.innerHTML = item.product_name;
-    description.appendChild(span);
+        let image = document.createElement("img");
+        image.classList.add("product_image")
+        image.setAttribute("src", item.image);
+        product.appendChild(image);
 
-    let h = document.createElement("h3");
-    h.innerHTML = "$" + item.price;
-    description.appendChild(h);
+        let description = document.createElement("div");
+        description.classList.add("product_description");
 
-    product.appendChild(description);
+        let span = document.createElement("span");
+        span.innerHTML = item.product_name;
+        description.appendChild(span);
 
-    document.getElementById("products").appendChild(product);
+        let h = document.createElement("h3");
+        h.innerHTML = "$" + item.price;
+        description.appendChild(h);
+
+        product.appendChild(description);
+
+        document.getElementById("products").appendChild(product);
+    }
 }

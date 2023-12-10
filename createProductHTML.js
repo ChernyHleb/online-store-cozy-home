d06ab4a24c907511319248cfe5a46e10e7572998
ChernyHleb <popovarlol@gmail.com>
2023-12-10 19:43:09 +0300

    deleted tabs

--- createProductHTML.js
+++ createProductHTML.js
@@ -1,34 +1,34 @@
 function createProductHTML(item){
     let product = document.createElement("div");
-        product.classList.add("product", item.product_type);
+    product.classList.add("product", item.product_type);
 
-        let image = document.createElement("img");
-        image.classList.add("product_image")
-        image.setAttribute("src", item.image);
-        product.appendChild(image);
+    let image = document.createElement("img");
+    image.classList.add("product_image")
+    image.setAttribute("src", item.image);
+    product.appendChild(image);
 
-        let description = document.createElement("div");
-        description.classList.add("product_description");
+    let description = document.createElement("div");
+    description.classList.add("product_description");
 
-        let span = document.createElement("span");
-        span.innerHTML = item.name;
-        description.appendChild(span);
+    let span = document.createElement("span");
+    span.innerHTML = item.name;
+    description.appendChild(span);
 
-        let h = document.createElement("h3");
-        h.innerHTML = "$" + item.price;
-        description.appendChild(h);
+    let h = document.createElement("h3");
+    h.innerHTML = "$" + item.price;
+    description.appendChild(h);
 
-        product.appendChild(description);
+    product.appendChild(description);
 
-        product.setAttribute("name", item.name);
-        image.setAttribute("name", item.name);
-        span.setAttribute("name", item.name);
-        h.setAttribute("name", item.name);
-        description.setAttribute("name", item.name);
+    product.setAttribute("name", item.name);
+    image.setAttribute("name", item.name);
+    span.setAttribute("name", item.name);
+    h.setAttribute("name", item.name);
+    description.setAttribute("name", item.name);
 
-        product.onclick = (event) => {
-            console.log(event.target.getAttribute('name'));
-        };
+    product.onclick = (event) => {
+        toggle_single_product_popup(event.target);
+    };
 
-        document.getElementById("products").appendChild(product);
+    document.getElementById("products").appendChild(product);
 }
